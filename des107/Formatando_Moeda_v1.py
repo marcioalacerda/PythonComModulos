23f2778a29415b81ceb3ecab7a822b48fdffd0ed
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-22 23:31:08 -0300

    Adicionado uma função adicional chamada moeda, para mostrar valores com o valor monetário formatado

--- des107/Formatando_Moeda_v1.py
+++ des107/Formatando_Moeda_v1.py
@@ -1,9 +1,8 @@
-'''Crie um módulo chamado moeda.py que tenha as funções aumentar(), diminuir(), dobro() e metade().
-Faça também um programa que importe esse módulo e use algumas dessas funções.'''
+'''Adapte o codigo, criando uma função adicional chamada moeda() que consiga mostrar valores com o valor monetário formatado'''
 from des107 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}.')
-print(f'O dobro de {p} é {moeda.dobro(p)}.')
-print(f'Aumantando 10%, temos {moeda.aumentar(p, 10)}.')
-print(f'Reduzindo 13%, teos {moeda.diminuar(p, 13)}.')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}.')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}.')
+print(f'Aumantando 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}.')
+print(f'Reduzindo 13%, teos {moeda.moeda(moeda.diminuar(p, 13))}.')
--- des107/moeda.py
+++ des107/moeda.py
@@ -1,18 +1,23 @@
-def aumentar(preço, taxa):
+def aumentar(preço=0, taxa=0):
     res = preço + (preço * taxa/100)
     return res
 
 
-def diminuar(preço, taxa):
+def diminuar(preço=0, taxa=0):
     res = preço - (preço * taxa/100)
     return res
 
 
-def dobro(preço):
+def dobro(preço=0):
     res = preço * 2
     return res
 
 
-def metade(preço):
+def metade(preço=0):
     res = preço / 2
+    return res
+
+
+def moeda(preço=0, moeda='R$'):
+    res = f'{moeda}{preço:.2f}'.replace('.', ',')
     return res
